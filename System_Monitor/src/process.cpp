ebf1acb1cc80bd448c683f76ffd00cc8521d9a41
Eric Schrock <eric.schrock22@gmail.com>
2022-12-30 10:45:37 -0500

    Project review feedback

--- System_Monitor/src/process.cpp
+++ System_Monitor/src/process.cpp
@@ -28,5 +28,7 @@ string Process::User() { return LinuxParser::User(pid); }
 long int Process::UpTime() { return LinuxParser::UpTime(pid); }
 
 bool Process::operator<(Process const& a) const {
-  return this->CpuUtilization() > a.CpuUtilization();
+  return this->CpuUtilization() >
+         a.CpuUtilization();  // Used ">" instead of "<" as an easy way to sort
+                              // largest to smallest in system.cpp
 }
